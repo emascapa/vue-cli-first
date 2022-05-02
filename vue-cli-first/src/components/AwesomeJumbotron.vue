ce6139ab6aca9c5add37f8671cd3be4e2319b9e5
emascapa <emascapa@gmail.com>
2022-05-02 17:06:25 +0200

    Commit

---
+++ vue-cli-first/src/components/AwesomeJumbotron.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="wrapper">
+        <img src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Fmobile%2F000%2F028%2F367%2Fcover1.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AwesomeJumbotron'
+}
+</script>
+
+<style scoped>
+
+    .wrapper {
+        margin: 2rem auto;
+        width: 70%;
+        max-width: 700px;
+        border-radius: 40px;
+        border: 3px solid #2c3e50;
+        overflow: hidden;
+    }
+
+    .wrapper > img {
+        display: block;
+        max-width: 100%;
+    }
+
+</style>
---
+++ vue-cli-first/src/components/AwesomeTitle.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div class="wrapper">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'AwesomeTitle'
+}
+</script>
+
+
+
+<style scoped>
+    .wrapper {
+        margin: 3rem auto;
+        padding: 1rem 0;
+        background-color: rgb(209, 223, 234);
+        width: 60%;
+        max-width: 700px;
+        border-radius: 40px;
+        border: 3px solid #2c3e50;
+    }
+
+    .wrapper > h1 {
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
